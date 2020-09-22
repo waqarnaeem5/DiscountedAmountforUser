f21b38ae7cec7fa17205ffb379a599a767b51c9e
Waqar Naeem <waqarnaeem2@hotmail.com>
2020-09-22 16:48:52 +0500

    Create ConstantFile.java

---
+++ ConstantFile.java
@@ -0,0 +1,20 @@
+import System;
+import System.Collections.Generic;
+import System.Text;
+
+namespace PaymentwiithDiscount {
+    
+    class Constantfile {
+        
+        public const Decimal twentytwo = 22;
+        
+        public const Decimal ten = 10;
+        
+        public const double Pi = 3.14159;
+        
+        public const int SpeedOfLight = 300000;
+        
+        //  km per sec.
+        public const Decimal amount = 1000;
+    }
+}
